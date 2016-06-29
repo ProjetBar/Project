c21008203014c7cb2f6acedaaa2f7831598b7c81
Remyy <remy.castells@gmail.com>
2016-06-29 15:58:40 +0200

    modification taille de la SDL

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ void init_SDL (void)
 	SDL_Rect position_Client; //position du client
 	SDL_Rect position_Barman;	// position du barman
 	SDL_Rect positionFond;
-	ecran = SDL_SetVideoMode(646, 716, 32, SDL_HWSURFACE);
+	ecran = SDL_SetVideoMode(984, 553, 32, SDL_HWSURFACE);
 	int avanceX = 1, avanceY = 1;
 	SDL_WM_SetCaption("Project Bar", NULL);
 	imageDeFond = SDL_LoadBMP("Bar.bmp");
